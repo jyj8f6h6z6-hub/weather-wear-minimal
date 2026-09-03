d377efb966e8cc111f4d575b4ec38c18649629cd
Ruei Yi <294120879+jyj8f6h6z6-hub@users.noreply.github.com>
2026-09-03 16:49:35 +0800

    修改

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Google Maps Platform 設定
 // 將你自己的 API Key 貼到下方；不要把私人 Key 傳給別人。
 window.APP_CONFIG = {
-  GOOGLE_MAPS_API_KEY: 'YOUR_GOOGLE_MAPS_API_KEY'
+  GOOGLE_MAPS_API_KEY: 'AIzaSyDHJ18rC0CuG4OGUbF-c4H7xavGoKOrTgw'
 };
